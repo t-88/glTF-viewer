84f0f6b3bf7dc290845d53eb5535bb0ebc994bca
t-88 <temptaki5002@gmail.com>
2024-02-07 16:09:07 +0100

    Create mesh class, first render

--- main.cpp
+++ main.cpp
@@ -20,9 +20,59 @@
 static char shader_log[512];
 static int shader_status;
 
+std::vector<float> vert = {
+    0.0, 0.4,  0.0,
+    -0.5, -0.5, 0.0,
+    0.5,  -0.5,  0.0,
+};
+std::vector<float> mesh_vertices;
+
+
+class Mesh {
+public:
+    uint32_t vao;
+    uint32_t vbo; 
+    uint32_t ebo; 
+
+    int vertices_count = 0;
+    std::vector<float> vertices;
+    std::vector<uint16_t> indices;
+    
+    Mesh(std::vector<float> _vertices,std::vector<uint16_t> _indices,int _vertices_count): vertices_count(_vertices_count) {
+        vertices = _vertices;
+        indices = _indices;
+
+        glGenVertexArrays(1,&vao);
+        glGenBuffers(1,&vbo);
+        glGenBuffers(1,&ebo);
+
+        glBindVertexArray(vao);
+    
+        glBindBuffer(GL_ARRAY_BUFFER,vbo); 
+        glBufferData(GL_ARRAY_BUFFER,vertices.size() * sizeof(vertices[0]),vertices.data(),GL_STATIC_DRAW);
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float) * 3,0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.size() * sizeof(indices[0]),indices.data(),GL_STATIC_DRAW);
+
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
+        glBindBuffer(GL_ARRAY_BUFFER,0);
+        glBindVertexArray(0);
+    }
+    ~Mesh() {}
+
+
+    void render() {
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES,indices.size(),GL_UNSIGNED_SHORT,indices.data());
+    }
+
+};
+
 
 void on_window_resize(GLFWwindow* window, int w, int h) {}
-
 int main() {
     Json::Value json;
 
@@ -40,10 +90,20 @@ int main() {
     Json::Value& primitive = json["meshes"][0]["primitives"][0];         
     Json::Value& position_accessor = json["accessors"][primitive["attributes"]["POSITION"].asInt()];
     Json::Value& buffer_view = json["bufferViews"][position_accessor["bufferView"].asInt()];
-    float* buffer = (float*)(bin_char.data() + buffer_view["byteOffset"].asInt());
-    printf("%d\n",position_accessor["count"].asInt());
+    float* buffer = (float*)(bin_char.data() + buffer_view["byteOffset"].asInt() + position_accessor["byteOffset"].asInt() );
+
     for (size_t i = 0; i < position_accessor["count"].asInt(); i++){
-        printf("%f %f %f\n",buffer[i * 3] , buffer[i * 3 + 1] , buffer[i * 3 + 2]);
+        mesh_vertices.push_back(buffer[i * 3 + 0]);
+        mesh_vertices.push_back(buffer[i * 3 + 1]);
+        mesh_vertices.push_back(0);
+    }
+
+    std::vector<uint16_t> mesh_indices;
+    Json::Value& indices_accessor = json["accessors"][0];
+    Json::Value& indices_buffer_view = json["bufferViews"][indices_accessor["bufferView"].asInt()];
+    uint16_t* buf = (uint16_t*)(bin_char.data() + indices_buffer_view["byteOffset"].asInt());
+    for (size_t i = 0; i < indices_accessor["count"].asInt(); i++){
+        mesh_indices.push_back(buf[i]);
     }
 
 
@@ -73,38 +133,10 @@ int main() {
 
     Shader shader("./shader.vert","./shader.frag");
 
-    float vertices[] = {
-        0.0, 0.4,  0.0,
-        -0.5, -0.5, 0.0,
-        0.5,  -0.5,  0.0,
-    };
 
-    uint32_t indices[] = {
-        0 , 1 , 2,
-    };
+    uint32_t indices[] = {0 , 1 , 2,};
 
-
-    uint32_t vao;
-    glGenVertexArrays(1,&vao);
-    uint32_t vbo; 
-    glGenBuffers(1,&vbo);
-    uint32_t ebo;
-    glGenBuffers(1,&ebo);
-
-    glBindVertexArray(vao);
-
-    glBindBuffer(GL_ARRAY_BUFFER,vbo); // vertex buffer
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(float) * 3,0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
-    glBindBuffer(GL_ARRAY_BUFFER,0);
-    glBindVertexArray(0);
-
+    Mesh mesh(mesh_vertices,mesh_indices,mesh_vertices.size() / 3);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -114,9 +146,7 @@ int main() {
         }
 
         shader.enable();
-        // glUseProgram(shader_program);
-        glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_INT,indices);
+        mesh.render();
 
 
         glfwSwapBuffers(window);
