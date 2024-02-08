141e77e478b58334114d41c57bf78ba344d61ffd
t-88 <temptaki5002@gmail.com>
2024-02-08 20:27:18 +0100

    Read normals out of the gltf

--- include/gltf_loader.hpp
+++ include/gltf_loader.hpp
@@ -8,6 +8,9 @@ public:
     Json::Value json;
     std::vector<float> vertices;
     std::vector<uint16_t> indices;
+    std::vector<float> normals;
+
+
     GltfLoader(const char* gltf_path,const char* gltf_bin);
     ~GltfLoader();
 }; 
--- src/gltf_loader.cpp
+++ src/gltf_loader.cpp
@@ -14,23 +14,34 @@ GltfLoader::GltfLoader(const char *gltf_path, const char *gltf_bin)
     bin_file.close();
 
     Json::Value &primitive = json["meshes"][0]["primitives"][0];
+
     Json::Value &position_accessor = json["accessors"][primitive["attributes"]["POSITION"].asInt()];
     Json::Value &buffer_view = json["bufferViews"][position_accessor["bufferView"].asInt()];
-    float *buffer = (float *)(bin_char.data() + buffer_view["byteOffset"].asInt() + position_accessor["byteOffset"].asInt());
-    for (size_t i = 0; i < position_accessor["count"].asInt(); i++)
-    {
-        vertices.push_back(buffer[i * 3 + 0]);
-        vertices.push_back(buffer[i * 3 + 1]);
-        vertices.push_back(buffer[i * 3 + 2]);
+    float *pos_buf = (float *)(bin_char.data() + buffer_view["byteOffset"].asInt() + position_accessor["byteOffset"].asInt());
+    for (size_t i = 0; i < position_accessor["count"].asInt(); i++) {
+        vertices.push_back(pos_buf[i * 3 + 0]);
+        vertices.push_back(pos_buf[i * 3 + 1]);
+        vertices.push_back(pos_buf[i * 3 + 2]);
     }
 
     Json::Value &indices_accessor = json["accessors"][0];
     Json::Value &indices_buffer_view = json["bufferViews"][indices_accessor["bufferView"].asInt()];
-    uint16_t *buf = (uint16_t *)(bin_char.data() + indices_buffer_view["byteOffset"].asInt());
-    for (size_t i = 0; i < indices_accessor["count"].asInt(); i++)
-    {
-        indices.push_back(buf[i]);
+    uint16_t *indices_buf = (uint16_t *)(bin_char.data() + indices_buffer_view["byteOffset"].asInt());
+    for (size_t i = 0; i < indices_accessor["count"].asInt(); i++) {
+        indices.push_back(indices_buf[i]);
     }
+
+
+    Json::Value &normals_accessor = json["accessors"][primitive["attributes"]["NORMAL"].asInt()];
+    Json::Value &normals_buffer_view = json["bufferViews"][normals_accessor["bufferView"].asInt()];
+    float *normals_buf = (float *)(bin_char.data() + normals_buffer_view["byteOffset"].asInt() + normals_accessor["byteOffset"].asInt());
+    for (size_t i = 0; i < normals_accessor["count"].asInt(); i++) {
+        normals.push_back(normals_buf[i * 3 + 0]);
+        normals.push_back(normals_buf[i * 3 + 1]);
+        normals.push_back(normals_buf[i * 3 + 2]);
+    }
+
+
 }
 
 
